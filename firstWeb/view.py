369bde2f82ca00385c5c210ffb7c2db8de1d8839
Äsif parvez <asifparvezsarker26@gmail.com>
2018-09-01 22:49:30 +0600

    Add html using template

--- firstWeb/view.py
+++ firstWeb/view.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
 
-	return HttpResponse('<h1>hellow</h1>')
+	return render(request,'index.html')
 
 def blog(request):
 
